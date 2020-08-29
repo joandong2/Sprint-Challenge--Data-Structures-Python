ee43a5bce86805ff05ed240e91acf8fc511ae27e
John Oblenda <joandong2@gmail.com>
2020-08-29 14:42:16 -0400

    task 1 removed tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -9,32 +9,30 @@ class RingBuffer:
             self.ring.append(item)
         else:
             self.ring[self.oldest_node] = item
-            self.oldest_node += 1
+            #self.oldest_node += 1
+        self.oldest_node = (self.oldest_node + 1) % self.capacity
         # print(self.oldest_node)
-        # 1 % 3 = 1 oldest = 0 + 1
-        # 2 % 3 = 2 oldest = 1 + 1
-        # 3 % 3 = 0 oldest = 2 + 1
 
     def get(self):
         return [item for item in self.ring]
 
 
-buffer = RingBuffer(3)
+# buffer = RingBuffer(3)
 
-buffer.get()   # should return []
+# buffer.get()   # should return []
 
-buffer.append('a')
-buffer.append('b')
-buffer.append('c')
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
 
-print(buffer.get())   # should return ['a', 'b', 'c']
+# print(buffer.get())   # should return ['a', 'b', 'c']
 
-# 'd' overwrites the oldest value in the ring buffer, which is 'a'
-buffer.append('d')
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
 
-print(buffer.get())    # should return ['d', 'b', 'c']
+# print(buffer.get())    # should return ['d', 'b', 'c']
 
-buffer.append('e')
-buffer.append('f')
+# buffer.append('e')
+# buffer.append('f')
 
-print(buffer.get())    # should return ['d', 'e', 'f']
+# print(buffer.get())    # should return ['d', 'e', 'f']
